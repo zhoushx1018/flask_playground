3e9e387a5d4ab1f74c7d8c22567cb35030586b67
zhoushx <zhoushx1018@gmail.com>
2020-05-25 21:03:52 +0800

    url_for

--- hello_world/__init__.py
+++ hello_world/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import url_for, escape
 app = Flask(__name__)
 
 
@@ -11,3 +12,8 @@ def hello():
 def hello_user(name):
     return 'hi, %s' % name
 
+@app.route('/test')
+def test_url_for():
+    print(url_for('hello'))
+    print(url_for('hello_user', name='steveZhou'))
+    return 'Test page'
