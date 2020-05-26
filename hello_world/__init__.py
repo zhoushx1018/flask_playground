57ff6790506619683c32f132c44dd67dfe2384fb
zhoushx <zhoushx1018@gmail.com>
2020-05-26 18:22:04 +0800

    handle form add, update

--- hello_world/__init__.py
+++ hello_world/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask,render_template
 from flask import url_for, escape
+from flask import request, redirect, flash
+
 from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
 import os
 import click
@@ -7,6 +9,7 @@ import click
 app = Flask(__name__)
 db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
+app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
 
 name = 'Steve Zhou'
 movies = [
@@ -33,13 +36,34 @@ class Movie(db.Model):  # 表名将会是 movie
     year = db.Column(db.String(4))  # 电影年份
 
 
-@app.route('/')
-def hello():
-    return '<h1>hello world</h1>'
+# @app.route('/')
+# def hello():
+#     return '<h1>hello world</h1>'
 
-@app.route('/watchlist')
+# @app.route('/watchlist')
+# def index():
+#     return render_template('index.html', name=name, movies=movies)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    if request.method == 'POST':  # 判断是否是 POST 请求
+        # 获取表单数据
+        title = request.form.get('title')  # 传入表单对应输入字段的 name 值
+        year = request.form.get('year')
+        # 验证数据
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')  # 显示错误提示
+            return redirect(url_for('index'))  # 重定向回主页
+        # 保存表单数据到数据库
+        movie = Movie(title=title, year=year)  # 创建记录
+        db.session.add(movie)  # 添加到数据库会话
+        db.session.commit()  # 提交数据库会话
+        flash('Item created.')  # 显示成功创建的提示
+        return redirect(url_for('index'))  # 重定向回主页
+
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
 
 
 @app.route('/user/<name>')
@@ -102,8 +126,36 @@ def page_not_found(e):  # 接受异常对象作为参数
     # return render_template('404.html', user=user), 404  # 返回模板和状态码
     return render_template('404.html'), 404  # 返回模板和状态码
 
+@app.errorhandler(405)
+def page_not_allowed(e):  # 接受异常对象作为参数
+    user = User.query.first()
+    # return render_template('404.html', user=user), 404  # 返回模板和状态码
+    return render_template('405.html'), 405  # 返回模板和状态码
+
 @app.context_processor
 def inject_user():  # 函数名可以随意修改
     user = User.query.first()
     return dict(user=user)  # 需要返回字典，等同于return {'user': user}
 
+
+
+
+@app.route('/movie/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+
+    if request.method == 'POST':  # 处理编辑表单的提交请求
+        title = request.form['title']
+        year = request.form['year']
+
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('edit', movie_id=movie_id))  # 重定向回对应的编辑页面
+
+        movie.title = title  # 更新标题
+        movie.year = year  # 更新年份
+        db.session.commit()  # 提交数据库会话
+        flash('Item updated.')
+        return redirect(url_for('index'))  # 重定向回主页
+
+    return render_template('edit.html', movie=movie)  # 传入被编辑的电影记录
