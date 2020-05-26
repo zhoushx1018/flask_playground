ea03ea334e3ea22fa88e49917dc414609992807a
zhoushx <zhoushx1018@gmail.com>
2020-05-26 16:01:05 +0800

    app.context_processor

--- hello_world/__init__.py
+++ hello_world/__init__.py
@@ -99,4 +99,11 @@ def index_from_db():
 @app.errorhandler(404)  # 传入要处理的错误代码
 def page_not_found(e):  # 接受异常对象作为参数
     user = User.query.first()
-    return render_template('404.html', user=user), 404  # 返回模板和状态码
+    # return render_template('404.html', user=user), 404  # 返回模板和状态码
+    return render_template('404.html'), 404  # 返回模板和状态码
+
+@app.context_processor
+def inject_user():  # 函数名可以随意修改
+    user = User.query.first()
+    return dict(user=user)  # 需要返回字典，等同于return {'user': user}
+
